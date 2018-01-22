d35e3a2c2d40d53d65606e1b702840063409a2cf
panupong <tayalone@gmail.com>
2018-01-22 10:04:37 +0700

    use fore cast api and use statusCode for check callback error

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const request = require('request')
 const yargs = require('yargs')
 
 const geocode = require('./geocode/geocode')
@@ -27,3 +26,20 @@ geocode.geoAddress(argv.address, (error, results) => {
     }
 })
 
+const request = require('request')
+const forcast = `https://api.darksky.net/forecast/ad3d8afd047de8e016955df585f28c2f/37.8267,-122.423`
+
+request({
+    url: forcast,
+    json: true
+}, (error, response, body) => {
+    if (error){
+        console.log(`Unable connect`)
+    } 
+    else if (response.statusCode  === 400) {
+        console.log(`Unable fetch data`)
+    }
+    else if (!error && response.statusCode === 200) {
+        console.log(body.currently.temperature)
+    }
+})
