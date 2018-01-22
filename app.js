96e85517705264e56ba831ff8e7dc0b77498d9ab
panupong <tayalone@gmail.com>
2018-01-22 09:18:51 +0700

    use encodeURIComponent create query param for googlemap api

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const request = require('request')
+const yargs = require('yargs')
+
+const argv = yargs
+    .options({
+        a : {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv
+
+console.log(argv.address)
+// address=%20chumsaeng%20nakhonsawan
+
+const address = encodeURIComponent(` ${argv.address}`) 
+
+
 
 request({
-    url: 'http://maps.googleapis.com/maps/api/geocode/json?address=%20chumsaeng%20nakhonsawan',
+    url: 'http://maps.googleapis.com/maps/api/geocode/json?address='+address,
     json: true
 }, (error, response, body) => {
     // console.log(JSON.stringify(body, undefined, 2))
